e254bce8d876723256bf2eaf7dc34f5ecda9714c
shubhambhatt19 <shubhambhatt19>
2021-10-24 17:26:22 +0000

    Update

--- model.py
+++ model.py
@@ -6,31 +6,43 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Conv2D, Dense, Flatten
 from functools import partial
 import efficientnet.tfkeras as efn
-
+import matplotlib.pyplot as plt
+import sys
+import cv2
 
 os.environ['TF_ENABLE_AUTO_MIXED_PRECISION'] = '1'
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 BATCH_SIZE = 16
-tf.compat.v1.enable_eager_execution()
+
 
 tfrecord_path = "/home/deploy/shubham/pratice/classification/train.record"
 CLASSES = 67
 IMAGE_SIZE = [120, 120]
 
+
+
+print(sys.argv)
+
+
 def decode_image(image):
-	image = tf.image.decode_jpeg(image, channels = 3)
+	# image = tf.image.decode_jpeg(image, channels = 3)
+	print(image)
+	# image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+	image = tf.io.decode_jpeg(image)
 	image = tf.cast(image, tf.float32)
-	image = tf.image.resize_images(image, [120, 120])
+	image = tf.compat.v1.image.resize(image, [120,120])
 	return image
 
 def read_tfrecord(example):
 	tfrecord_format = ({
 		"image": tf.io.FixedLenFeature([], tf.string),
-		"label": tf.io.FixedLenFeature([], tf.string)
+		"label": tf.io.FixedLenFeature([], tf.int64)
 		})
 	example = tf.io.parse_single_example(example, tfrecord_format)
 	x_train = decode_image(example["image"])
 	label = example["label"]
+	# label = tf.nn.embedding_lookup(np.identity(10), label)
+	label = tf.one_hot(label, depth = CLASSES)
 	return x_train,label
 
 def load_dataset(filename):
@@ -55,18 +67,20 @@ image_train, label_train = next(iter(train))
 initial_learning_rate = 0.1
 lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate, decay_steps = 20, decay_rate = 0.95, staircase = True)
 
+
+
+
 def make_model():
     base_model = tf.keras.applications.Xception(input_shape=(*IMAGE_SIZE, 3), include_top=False, weights="imagenet")
-
     base_model.trainable = False
-
     inputs = tf.keras.layers.Input([*IMAGE_SIZE, 3])
     x = tf.keras.applications.xception.preprocess_input(inputs)
     x = base_model(x)
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
     x = tf.keras.layers.Dense(8, activation="relu")(x)
     x = tf.keras.layers.Dropout(0.7)(x)
-    outputs = tf.keras.layers.Dense(CLASSES, activation="sigmoid")(x)
+    x = tf.keras.layers.Flatten()(x)
+    outputs = tf.keras.layers.Dense(67, activation="softmax")(x)
 
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr_schedule),loss="categorical_crossentropy",metrics=["accuracy"])
@@ -74,7 +88,19 @@ def make_model():
 
 model = make_model()
 
-history = model.fit(train,epochs=2)
+history = model.fit(image_train, label_train,epochs=20, verbose = 1)
 
-model.summary()
 
+print("#####################################")
+print("#####################################")
+print(model.summary())
+print("#####################################")
+print("#####################################")
+
+
+### Evaluating on the train dataset
+model.evaluate(image_train,label_train)
+
+# Checking the graph for the accuracy and the loss throughout the training
+plt.plot(history.history["accuracy"])
+plt.plot(history.history["loss"])
--- record_file_creation.py
+++ record_file_creation.py
@@ -5,30 +5,55 @@ import cv2
 from object_detection.utils import dataset_util
 import os
 
+lbl = ["pantry","laundromat","dentaloffice","classroom","laboratorywet","hospitalroom","nursery","bookstore","gym","kindergarden","hairsalon","operating_room","casino","office","corridor","greenhouse","museum","concert_hall","elevator","warehouse","restaurant","waitingroom","locker_room","toystore","bakery","stairscase","bathroom","bar","trainstation","children_room","jewelleryshop","videostore","airport_inside","church_inside","winecellar","buffet","garage","inside_bus","shoeshop","livingroom","bedroom","poolinside","closet","dining_room","meeting_room","bowling","studiomusic","clothingstore","grocerystore","cloister","restaurant_kitchen","kitchen","gameroom","subway","deli","artstudio","fastfood_restaurant","florist","auditorium","computerroom","mall","tv_studio","movietheater","lobby","library","inside_subway","prisoncell"]
+lbl_ = {}
+
+for i,j in enumerate(lbl, start=0):
+	lbl_[j] = i
+
+
+print("label",lbl_)
+
+
+# tf.compat.v1.enabl
 with open("TrainImages.txt") as f:
 	train_images = f.readlines()
 
+def image_feature(value):
+    """Returns a bytes_list from a string / byte."""
+    return tf.train.Feature(
+        bytes_list=tf.train.BytesList(value=[tf.io.encode_jpeg(value).numpy()]))
+
+def bytes_feature(value):
+    """Returns a bytes_list from a string / byte."""
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value.encode()]))
+
+def int64_feature(value):
+    """Returns an int64_list from a bool / enum / int / uint."""
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
 
 def encode_image(path):
-	image = cv2.imread(path)
-	image = cv2.imencode('.jpg',image)[1].tostring()
+	# image = cv2.imread(path)
+	image = tf.keras.utils.load_img(path)
 	return image
 	
-writer = tf.python_io.TFRecordWriter("/home/deploy/shubham/pratice/classification/train.record")
+writer = tf.io.TFRecordWriter("/home/deploy/shubham/pratice/classification/train.record")
 
 for i in train_images:
 	path_ = os.path.join("/home/deploy/shubham/pratice/classification/indoorCVPR_09/Images",i.strip())
 	
-	try:
-		encoded_img = encode_image(path_)
-	except:
-		pass
+	# try:
+	encoded_img = encode_image(path_)
+	# except:
+	# 	print(path_)
+	# 	continue
 
-	label = i.split("/")[0]
-	label = tf.compat.as_bytes(label)
+	label = lbl_[i.split("/")[0]]
+	# label = tf.compat.as_bytes(label)
 
 	tf_example = tf.train.Example(features = tf.train.Features(feature = {
-		"image":dataset_util.bytes_feature(encoded_img),
-		"label":dataset_util.bytes_feature(label)
+		"image":image_feature(encoded_img),
+		"label":int64_feature(label)
 		}))
 	writer.write(tf_example.SerializeToString())
